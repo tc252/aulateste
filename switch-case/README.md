9e0f8fa67e4a5923f23f5575197af2d440f75ec7
tc252 <contato.tereza25@gmail.com>
2024-10-16 08:25:08 -0300

    Add files via upload

---
+++ switch-case/README.md
@@ -0,0 +1,4 @@
+# Switch Case
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Tereza-Oliveira/pen/BaXQWzw](https://codepen.io/Tereza-Oliveira/pen/BaXQWzw).
+
---
+++ switch-case/dist/script.js
@@ -0,0 +1,12 @@
+const pesquisa = "";
+
+switch (pesquisa) {
+  case "maçã":
+    console.log("A maçã custa R$5,00");
+    break;
+  case "limão":
+    console.log("O limão custa R$4,00");
+    break;
+  default:
+    console.log("Essa fruta está indisponível");
+}
